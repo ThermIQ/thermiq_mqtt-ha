fb74e98dfe8786ab38d86efde9d32e3100d9d604
anders <anders@thermiq.net>
2022-12-08 00:07:52 +0100

    Added some basic checks to config_flow.py Updated README.md

--- custom_components/thermiq_mqtt/config_flow.py
+++ custom_components/thermiq_mqtt/config_flow.py
@@ -4,7 +4,8 @@ import voluptuous as vol
 from homeassistant import config_entries, exceptions
 from homeassistant.core import callback
 from homeassistant.helpers import config_validation as cv
-
+from homeassistant.helpers import selector
+from homeassistant.components.mqtt import valid_subscribe_topic
 
 from .const import (
     DOMAIN,
@@ -12,13 +13,13 @@ from .const import (
     CONF_MQTT_NODE,
     CONF_MQTT_HEX,
     CONF_MQTT_DBG,
-    CONF_MQTT_LANGUAGE,
+    CONF_LANGUAGE,
     AVAILABLE_LANGUAGES,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
-#ToDo:
+# ToDo:
 #   Add check of Nodename
 #   Select list of languages
 #   check ID to be spaceless+[a-z/A-Z/0-9]
@@ -48,7 +49,12 @@ class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             {
                 vol.Required(CONF_ID, default="vp1"): cv.string,
                 vol.Required(CONF_MQTT_NODE, default="ThermIQ/ThermIQ-mqtt"): cv.string,
-                vol.Optional(CONF_MQTT_LANGUAGE, default="en"): cv.string,
+                vol.Required(CONF_LANGUAGE, default="en"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(
+                        options=["en", "se", "fi", "no", "de"],
+                        mode=selector.SelectSelectorMode.DROPDOWN,
+                    ),
+                ),
                 vol.Required(CONF_MQTT_HEX, default=False): cv.boolean,
                 vol.Required(CONF_MQTT_DBG, default=False): cv.boolean,
             }
@@ -64,9 +70,14 @@ class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     vol.Required(
                         CONF_MQTT_NODE, default=user_input[CONF_MQTT_NODE]
                     ): cv.string,
-                    vol.Optional(
-                        CONF_MQTT_LANGUAGE, default=user_input[CONF_MQTT_LANGUAGE]
-                    ): cv.string,
+                    vol.Required(
+                        CONF_LANGUAGE, default=user_input[CONF_LANGUAGE]
+                    ): selector.SelectSelector(
+                        selector.SelectSelectorConfig(
+                            options=["en", "se", "fi", "no", "de"],
+                            mode=selector.SelectSelectorMode.DROPDOWN,
+                        ),
+                    ),
                     vol.Required(
                         CONF_MQTT_HEX, default=user_input[CONF_MQTT_HEX]
                     ): cv.boolean,
@@ -78,6 +89,23 @@ class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
             try:
                 id_name = user_input[CONF_ID]
+                unique_id = f"{DOMAIN}_{id_name}"
+                await self.async_set_unique_id(unique_id)
+                self._abort_if_unique_id_configured()
+            except:
+                return self.async_show_form(
+                    step_id="user",
+                    data_schema=error_schema,
+                    errors={"base": "invalid_id"},
+                )
+
+            try:
+                prefix = user_input[CONF_MQTT_NODE]
+                if prefix.endswith("/#"):
+                    prefix = prefix[:-2]
+                elif prefix.endswith("/"):
+                    prefix = prefix[:-1]
+                valid_subscribe_topic(f"{prefix}/#")
             except:
                 return self.async_show_form(
                     step_id="user",
@@ -86,7 +114,7 @@ class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 )
 
             try:
-                lang = AVAILABLE_LANGUAGES.index(user_input[CONF_MQTT_LANGUAGE])
+                lang = AVAILABLE_LANGUAGES.index(user_input[CONF_LANGUAGE])
 
             except:
                 return self.async_show_form(
@@ -97,17 +125,12 @@ class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
             try:
 
-                unique_id = f"{DOMAIN}_{id_name}"
-                await self.async_set_unique_id(unique_id)
-                self._abort_if_unique_id_configured()
-
-
                 return self.async_create_entry(
                     title=unique_id,
                     data={
                         CONF_ID: id_name,
-                        CONF_MQTT_NODE: user_input[CONF_MQTT_NODE],
-                        CONF_MQTT_LANGUAGE: user_input[CONF_MQTT_LANGUAGE],
+                        CONF_MQTT_NODE: prefix,
+                        CONF_LANGUAGE: user_input[CONF_LANGUAGE],
                         CONF_MQTT_HEX: user_input[CONF_MQTT_HEX],
                         CONF_MQTT_DBG: user_input[CONF_MQTT_DBG],
                     },
@@ -143,16 +166,22 @@ class OptionsFlow(config_entries.OptionsFlow):
         return data
 
     async def async_step_user(self, user_input=None):
+        s = self.config_entry.data.get(CONF_LANGUAGE)
 
         data_schema = vol.Schema(
             {
                 vol.Required(
                     CONF_MQTT_NODE, default=self.config_entry.data.get(CONF_MQTT_NODE)
                 ): cv.string,
-                vol.Optional(
-                    CONF_MQTT_LANGUAGE,
-                    default=self.config_entry.data.get(CONF_MQTT_LANGUAGE),
-                ): cv.string,
+                vol.Required(
+                    CONF_LANGUAGE,
+                    default=self.config_entry.data.get(CONF_LANGUAGE),
+                ): selector.SelectSelector(
+                    selector.SelectSelectorConfig(
+                        options=["en", "se", "fi", "no", "de"],
+                        mode=selector.SelectSelectorMode.DROPDOWN,
+                    ),
+                ),
                 vol.Required(
                     CONF_MQTT_HEX, default=self.config_entry.data.get(CONF_MQTT_HEX)
                 ): cv.boolean,
@@ -170,9 +199,14 @@ class OptionsFlow(config_entries.OptionsFlow):
                     vol.Required(
                         CONF_MQTT_NODE, default=user_input[CONF_MQTT_NODE]
                     ): cv.string,
-                    vol.Optional(
-                        CONF_MQTT_LANGUAGE, default=user_input[CONF_MQTT_LANGUAGE]
-                    ): cv.string,
+                    vol.Required(
+                        CONF_LANGUAGE, default=user_input[CONF_LANGUAGE]
+                    ): selector.SelectSelector(
+                        selector.SelectSelectorConfig(
+                            options=["en", "se", "fi", "no", "de"],
+                            mode=selector.SelectSelectorMode.DROPDOWN,
+                        ),
+                    ),
                     vol.Required(
                         CONF_MQTT_HEX, default=user_input[CONF_MQTT_HEX]
                     ): cv.boolean,
@@ -185,12 +219,43 @@ class OptionsFlow(config_entries.OptionsFlow):
             try:
                 entryTitle = self.config_entry.title
                 id_name = self.config_entry.data[CONF_ID]
-                lang = AVAILABLE_LANGUAGES.index(user_input[CONF_MQTT_LANGUAGE])
+            except:
+                return self.async_show_form(
+                    step_id="user",
+                    data_schema=error_schema,
+                    errors={"base": "invalid_id"},
+                )
 
+            try:
+                prefix = user_input[CONF_MQTT_NODE]
+                if prefix.endswith("/#"):
+                    prefix = prefix[:-2]
+                elif prefix.endswith("/"):
+                    prefix = prefix[:-1]
+                valid_subscribe_topic(f"{prefix}/#")
+
+            except:
+                return self.async_show_form(
+                    step_id="user",
+                    data_schema=error_schema,
+                    errors={"base": "invalid_nodename"},
+                )
+
+            try:
+                lang = AVAILABLE_LANGUAGES.index(user_input[CONF_LANGUAGE])
+
+            except:
+                return self.async_show_form(
+                    step_id="user",
+                    data_schema=error_schema,
+                    errors={"base": "invalid_language"},
+                )
+
+            try:
                 data = {
                     CONF_ID: id_name,
-                    CONF_MQTT_NODE: user_input[CONF_MQTT_NODE],
-                    CONF_MQTT_LANGUAGE: user_input[CONF_MQTT_LANGUAGE],
+                    CONF_MQTT_NODE: prefix,
+                    CONF_LANGUAGE: user_input[CONF_LANGUAGE],
                     CONF_MQTT_HEX: user_input[CONF_MQTT_HEX],
                     CONF_MQTT_DBG: user_input[CONF_MQTT_DBG],
                 }
